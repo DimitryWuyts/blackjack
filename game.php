90b84908b562aeab21cf7caec7b3ece52cdf9dd4
dimitry <dimiw1337@hotmail.com>
2020-02-13 10:58:00 -0500

    works just one little problem still with when i stand it doesnt display yet, but almost done!

--- game.php
+++ game.php
@@ -1,7 +1,109 @@
 <?php
+//enable strict typing
+declare(strict_types=1);
 require "home.php";
 
 session_start();
+
+//enable errors
+ini_set('display_errors', '1');
+ini_set('display_startup_errors', '1');
+error_reporting(E_ALL);
+
+class Blackjack
+{
+
+    const LOW_CARD = 1;
+    const HIGH_CARD = 11;
+    private $score = 0;
+
+//contstructor for score
+
+    public function __construct(int $score)
+    {
+        $this->score = $score;
+    }
+//--
+    public function hit() {
+        $card = rand(self::LOW_CARD, self::HIGH_CARD);
+        $this->score += $card;
+    }
+
+    public function getScore(): int
+    {
+        return $this->score;
+    }
+//dealer
+    public function stand(Blackjack $Dealer, Blackjack $Player) {
+
+        do {
+            $Dealer->hit();
+            $_SESSION["Dealer"] = $Dealer->getScore();
+        }
+        while
+        ($Dealer->getScore() < 15);
+
+        if ($Dealer->getScore() > 21){
+            echo  "You won!";
+            session_destroy();
+        }
+
+        else if (($Player->getScore()) > ($Dealer->getScore())){
+            echo "You won!";
+            session_destroy();
+        }
+        else if (($Player->getScore()) == ($Dealer->getScore())){
+            echo  "Tie! Dealer wins!";
+            session_destroy();
+        }
+    }
+    public function surrender() {
+
+    }
+
+    }
+
+if (isset($_SESSION["Player"])) {
+    $Player = new Blackjack($_SESSION["Player"]);
+} else{
+    $_SESSION["Player"] = 0;
+    $Player = new Blackjack($_SESSION["Player"]);
+}
+
+if (isset($_SESSION["Dealer"])) {
+    $Dealer = new Blackjack($_SESSION["Dealer"]);
+} else {
+    $_SESSION["Dealer"] = 0;
+    $Dealer = new Blackjack($_SESSION["Dealer"]);
+}
+//player
+if (isset($_REQUEST['btn_submit'])) {
+    if ($_REQUEST['btn_submit'] == "HIT") {
+        $Player->hit();
+        $_SESSION["Player"] = $Player->getScore();
+
+        if ($Player->getScore() > 21){
+            echo "You lost";
+            session_destroy();
+        }
+        else if ($Player->getScore() == 21) {
+           echo "Blackjack! You won";
+            session_destroy();
+        }
+    }
+
+    if ($_REQUEST['btn_submit'] == "STAND") {
+        $Player->stand($Dealer,$Player);
+    }
+
+    if ($_REQUEST['btn_submit'] == "SURRENDER") {
+        $Player->surrender();
+        echo  "You lose";
+        session_destroy();
+    }
+}
+
+
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -20,22 +122,22 @@ session_start();
         <div class="col-md-12">
             <h3>PLAYER</h3>
             <p class="m-2">
-                You:
+                You: <?php echo $Player->getScore()?>
             </p>
         </div>
         <div class="col-md-12">
             <h3>DEALER</h3>
             <p class="m-2">
-                Dealer:
+                Dealer: <?php echo $Dealer->getScore()?>
             </p>
         </div>
     </div>
     <div class="row">
         <div class="col-md-3">
             <form action="game.php" method="POST" class="mr-3">
-                <input type="submit" name="btn_submit" value="Hit">
-                <input type="submit" name="btn_submit" value="Stand"/>
-                <input type="submit" name="btn_submit" value="Surrender">
+                <input type="submit" name="btn_submit" value="HIT">
+                <input type="submit" name="btn_submit" value="STAND"/>
+                <input type="submit" name="btn_submit" value="SURRENDER">
             </form>
         </div>
     </div>
