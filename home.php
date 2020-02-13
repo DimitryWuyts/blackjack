e52ff04ba856c53e6759e9941757f007ac82e9e8
dimitry <dimiw1337@hotmail.com>
2020-02-13 04:13:50 -0500

    button in home.php linking to game.php done

---
+++ home.php
@@ -0,0 +1,16 @@
+<?php ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
+    <title>Blackjack</title>
+</head>
+<body>
+<form action="game.php">
+    <input type="submit" value="Blackjack">
+</form>
+</body>
+</html>
